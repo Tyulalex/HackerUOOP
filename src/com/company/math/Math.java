bea22db396b4ae468078a98a4fff74d9cc337c73
tkarabut <tkarabut@aligntech.com>
2019-10-11 21:06:44 +0300

    math task

---
+++ src/com/company/math/Math.java
@@ -0,0 +1,45 @@
+package com.company.math;
+
+public class Math {
+
+    public static Integer convertToInteger(String value) {
+        return Integer.parseInt(value);
+    }
+
+    public static String compareInteger(Integer value1, Integer value2) {
+        int result = Integer.compare(value1, value2);
+        String compareResultString = "";
+        switch (result) {
+            case 1:
+                compareResultString = "Больше";
+                break;
+            case -1:
+                compareResultString = "Меньше";
+                break;
+            case 0:
+                compareResultString = "Равно";
+                break;
+        }
+        return compareResultString;
+    }
+
+    public static Integer pow(Integer value, int power) {
+        if (power == 0) {
+            value = 1;
+        } else if (power > 0) {
+            value = value * pow(value, power - 1);
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.convertToInteger("54"));
+        System.out.println(compareInteger(4, 3));
+        System.out.println(compareInteger(3, 3));
+        System.out.println(compareInteger(3, 4));
+
+        System.out.println(Math.pow(3, 3));
+    }
+}
+
+
