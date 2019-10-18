06d90344a59df86ded4acb6f9bafc4457ea72322
Tatiana Karabut <tyulalex@gmail.com>
2019-10-18 21:36:30 +0300

    update house

--- src/com/company/house/House.java
+++ src/com/company/house/House.java
@@ -7,18 +7,16 @@ public class House {
     private HouseElement[] doors;
     private HouseElement roof;
 
-    private HouseElement[][] houseElements;
-
     public House() {
         this.windows = new Window[]{new Window(), new Window(), new Window()};
         this.doors = new Door[]{new Door(), new Door()};
         this.roof = new Roof();
 
-        this.houseElements = new HouseElement[][]{windows, doors, {roof}};
     }
 
     void makeRepairs() {
-        for (HouseElement[] hs : this.houseElements) {
+        HouseElement[][] houseElements = {windows, doors, {roof}};
+        for (HouseElement[] hs : houseElements) {
             for (HouseElement houseElement : hs) {
                 houseElement.repair();
             }
