9ebb7b8a921f64d9ececfda819f8bd77c83a1a70
tyulalex <tyulalex@gmail.com>
2019-10-22 18:48:23 +0300

    petrol station

--- src/com/company/petrol_station/PetrolStation.java
+++ src/com/company/petrol_station/PetrolStation.java
@@ -4,9 +4,18 @@ public class PetrolStation {
 
     public void makeService() {
         Car car = new Car(200, TypeOfGas.GAS_98, 0.0f);
-        Order order = Cashbox.createOrder(car.getTypeOfGas(), car.getMoney());
+        Order order = Cashbox.createOrder(car);
         car.addGas(Dispenser.fillUp(order.getLitters()));
         System.out.println();
+
+        Car car2 = new Car(2500, TypeOfGas.GAS_95, 55);
+        try {
+            Order order1 = Cashbox.createOrderFullTank(car2);
+            car2.addGas(Dispenser.fillUp(order1.getLitters()));
+            System.out.println("Change: " + car2.getMoney());
+        } catch (Exception e) {
+            System.out.println("Unable to do service, not enough money");
+        }
     }
 
     public static void main(String[] args) {
